e3d357d1c3053da22fca14329d582fde6d6f6594
KaiserLancelot <1244713586@qq.com>
2020-05-13 00:16:43 +0800

    2020.5.13

--- cmake/Check.cmake
+++ cmake/Check.cmake
@@ -7,3 +7,26 @@ if(NOT (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES
                                                 "(Apple)?[Cc]lang"))
   message(FATAL_ERROR "Only supports GCC and Clang")
 endif()
+
+if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
+  message(
+    FATAL_ERROR
+      "In-source builds not allowed. Please make a new directory (called a build directory) and run CMake from there."
+  )
+endif()
+
+if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+  message(STATUS "Setting build type to 'Release' as none was specified")
+  set(CMAKE_BUILD_TYPE
+      "Release"
+      CACHE STRING "Choose the type of build" FORCE)
+endif()
+
+if(NOT COVERAGE)
+  if((CMAKE_BUILD_TYPE MATCHES "Debug") OR (CMAKE_BUILD_TYPE MATCHES
+                                            "RelWithDebInfo"))
+    set(COVERAGE ON)
+  else()
+    set(COVERAGE OFF)
+  endif()
+endif()
