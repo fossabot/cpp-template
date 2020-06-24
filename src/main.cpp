3183bf4e4df1b9e6af69a83ada074d35c7a0556c
kaiser <1244713586@qq.com>
2020-06-24 21:46:07 +0800

    2020.6.24

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
-
 #include "lib/add.h"
+
+#include <iostream>
 
 int main() { std::cout << add(1, 2) << '\n'; }
--- test/add_test.cpp
+++ test/add_test.cpp
@@ -1,5 +1,5 @@
-#include <gtest/gtest.h>
-
 #include "lib/add.h"
+
+#include <gtest/gtest.h>
 
 TEST(add_test, test1) { ASSERT_EQ(add(1, 2), 3); }
