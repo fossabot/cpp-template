6bb58aec216312fab8df6c7ab18273056b57ff71
kaiser <1244713586@qq.com>
2020-06-27 21:27:09 +0800

    2020.6.27

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include "lib/add.h"
 
+#include <cstdint>
 #include <iostream>
+#include <vector>
 
-int main() { std::cout << add(1, 2) << '\n'; }
+int main() {
+  std::vector<std::int32_t> v;
+  v.push_back(1);
+  v.push_back(2);
+  v.push_back(3);
+
+  std::cout << add(1, 2) << '\n';
+}
