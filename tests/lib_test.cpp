fe16f0d57eb1bb9b26c72b27f097e127d4229f77
kaiser <1244713586@qq.com>
2020-09-06 22:41:05 +0800

    2020.9.6

--- tests/lib_test.cpp
+++ tests/lib_test.cpp
@@ -9,3 +9,28 @@ TEST(lib_test, test1)
 
     ASSERT_EQ(add(1, 2), 3);
 }
+
+TEST(lib_test, test2)
+{
+    ASSERT_EQ(add(1, 2), 3);
+}
+
+TEST(lib_test, test3)
+{
+    ASSERT_EQ(add(1, 2), 3);
+}
+
+TEST(lib_test, test4)
+{
+    ASSERT_EQ(add(1, 2), 3);
+}
+
+TEST(lib_test, test5)
+{
+    ASSERT_EQ(add(1, 2), 3);
+}
+
+TEST(lib_test, test6)
+{
+    ASSERT_EQ(add(1, 2), 3);
+}
