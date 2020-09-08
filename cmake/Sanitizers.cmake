c467e9e9b02f040b2d0f64b5c4a037882a661735
kaiser <1244713586@qq.com>
2020-09-09 00:01:13 +0800

    2020.9.9

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -19,13 +19,14 @@ if(SANITIZER)
     endif()
 
     message(STATUS "Building with UndefinedSanitizer")
-    string(APPEND CMAKE_CXX_FLAGS " -fsanitize=undefined")
+    string(APPEND CMAKE_CXX_FLAGS
+           " -fsanitize=undefined -fno-sanitize-recover=all")
 
     if(CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?Clang")
       string(
         APPEND
         CMAKE_CXX_FLAGS
-        " -fsanitize=float-divide-by-zero -fsanitize=local-bounds -fsanitize=implicit-conversion -fsanitize=integer -fsanitize=nullability"
+        " -fsanitize=float-divide-by-zero -fsanitize=local-bounds -fsanitize=implicit-conversion -fsanitize=integer -fsanitize=nullability -fsanitize-recover=unsigned-integer-overflow"
       )
     endif()
   else()
